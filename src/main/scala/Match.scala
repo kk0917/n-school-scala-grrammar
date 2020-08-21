821539246702a31f10d83a443c6c1e5ccf864450
ko-kamenashi <kamenashi0917@gmail.com>
2020-08-21 21:30:27 +0900

    add pattern match samples

---
+++ src/main/scala/Match.scala
@@ -0,0 +1,70 @@
+object Match {
+  def intToName(num: Int): String = {
+    num match {
+      case 1 => "one"
+      case 2 => "two"
+      case _ => "other"
+    }
+  }
+
+  def message(message: String): String = {
+    message match {
+      case "good" | "bad" => "game"
+    }
+  }
+
+  def patternMatch(): Unit = {
+    val seq = Seq("A", "B", "C", "D", "E")
+
+    seq match {
+      case Seq("A", b, c, d, e) if b != "B" =>
+        println("b = " + b)
+        println("c = " + c)
+        println("d = " + d)
+        println("e = " + e)
+      case _ =>
+        println("nothing")
+    }
+  }
+
+  def patternMatchNest(): Unit = {
+    val seq = Seq(Seq("B"), Seq("B", "C", "D", "E"))
+
+    seq match {
+      case Seq(a@Seq("A"), x) =>
+        println(a)
+        println(x)
+      case _ => println("nothing")
+    }
+  }
+
+  def patternMatchType(): Unit = {
+    val obj: AnyRef = "String Literal"
+
+    obj match {
+      case v: java.lang.Integer => println("Integer!")
+      case v: String => println(s"String! length is ${v.length}")
+    }
+  }
+
+  def patternMatchGenerics(): Unit = {
+    val obj: AnyRef = Seq("a", "b", "c")
+    obj match {
+      case v: Seq[_] => println("Seq[_]")
+      case _ => println("nothing")
+    }
+  }
+
+  def printRandomChars(): Unit = {
+    for (i <- 1 to 1000) {
+      val chars: Seq[Char] = new scala.util.Random(new java.security.SecureRandom()).alphanumeric.take(5).toList
+
+      val result = chars match {
+        case c: Seq[_] if c(0) == c(4) => c
+        case _ => "Nothing..."
+      }
+
+      println(result)
+    }
+  }
+}
