ca0aa18c217314bdfb9065a8900195ba50ab9250
ko-kamenashi <kamenashi0917@gmail.com>
2020-08-24 22:00:51 +0900

    feature: add, update several features and minor fix
    * changed hero and monster default value
    * add hero and monster name
    * separate attack turn
    * add output pattern when inputting wrong command number
    * move attack definition from Hero class to Creature class

--- src/main/scala/RPG.scala
+++ src/main/scala/RPG.scala
@@ -3,56 +3,81 @@ import java.util.Random
 object RPG extends App {
   val random       = new Random()
   val monsterCount = 5
-  val hero         = new Hero("ポクお", 200, 30)
-  var monsters     = for (i <- 1 to monsterCount) yield new Monster(random.nextInt(120), random.nextInt(120), false)
+  val hero         = new Hero("ポクお", 200, 50)
+  var monsters     = for (i <- 1 to monsterCount) yield new Monster(random.nextInt(300), random.nextInt(120), false)
 
   println(
-    s"""あなたは冒険中の ${hero} であり、
-      |${monsterCount}匹のモンスターが潜んでいる洞窟を抜けねばならない。
+    s"""あなた、${hero.name}は冒険中のヒーローであり、
+      |モンスターが潜んでいる洞窟を抜けねばならない。
       |【ルール】:
       |1を入力してEnterキーを押すと攻撃、それ以外を入力すると逃走となる。
       |逃走成功確率は50%。逃走に失敗した場合はダメージをうける。
-      |一度でもダメージを受けるとモンスターの体力と攻撃力が判明する。
-      |またモンスターを倒した場合、武器を奪いその攻撃力を得ることできる。
+      |またモンスターを倒した場合、モンスターの武器の攻撃力が自分より高い場合はその武器を奪うことできる。
       |---------------------------------------------
-      |未知のモンスターがあらわれた。""".stripMargin)
+      |未知のモンスターが${monsterCount}匹あらわれた。""".stripMargin)
+  println(s"${ monsters.map { m: Monster => println(m.name) }}") // TODO: Shouldn't output lastline
+  println(s"\n【現在の状態】: ${hero}")
 
-  while (!monsters.isEmpty) {
+  while (monsters.nonEmpty) {
     val monster = monsters.head
-    val input   = scala.io.StdIn.readLine("【選択】: 攻撃[1] or 逃走[0] > ")
+    val input   = scala.io.StdIn.readLine(s"\n【選択】: 攻撃[1] or 逃走[0] > ")
 
     if (input == "1") { // 攻撃する
+      // hero's turn
       hero.attack(monster)
-      println(s"あなたは${hero.attackDamage}のダメージを与え、${monster.attackDamage}のダメージを受けた。")
+      println(s"${hero.name}は${hero.attackDamage}のダメージを与えた。")
 
-    } else { // 逃走する
-      if(hero.escape(monster)) {
-        println("あなたは、モンスターから逃走に成功した。")
+      // monster's turn
+      monster.attack(hero)
+
+      val message = if (monster.attackDamage == 0) {
+        s"${monster.name}は${hero.name}にダメージを与えられない。"
 
       } else {
-        println(s"あなたは、モンスターから逃走に失敗し、${monster.attackDamage}のダメージを受けた。")
+        s"${monster.name}は${hero.name}に${monster.attackDamage}のダメージを与えた。"
       }
-    }
 
-    println(s"【現在の状態】: ${hero}, ${monster}")
+      println(message)
+
+      println(s"【現在の状態】: ${hero}, ${monster}")
+
+    } else if (input == "0") { // 逃走する
+      if (hero.isEscape(monster)) {
+        println(s"${hero.name}は、モンスターから逃走に成功した。")
+        monster.isAwayFromHero = true
+        println(s"【現在の状態】: ${hero}")
+
+      } else {
+        monster.attack(hero)
+        println(s"${hero.name}は、モンスターから逃走に失敗し、${monster.attackDamage}のダメージを受けた。")
+        println(s"【現在の状態】: ${hero}, ${monster}")
+      }
+    } else {
+      println("入力に誤りがあります。")
+    }
 
     if (!hero.isAlive) { // Hero が死んでいるかどうか
       println(
         """---------------------------------------------
-          |【ゲームオーバー】: あなたは無残にも殺されてしまった。 """.stripMargin)
+          |【ゲームオーバー】: ${hero.name}は無残にも殺されてしまった。 """.stripMargin)
 
       System.exit(0)
 
     } else if (!monster.isAlive || monster.isAwayFromHero) { // Monster いないかどうか
       if(!monster.isAwayFromHero) { // 倒した場合
-        println("モンスターは倒れた。そしてあなたは、モンスターの武器を奪った。")
-        if (monster.attackDamage > hero.attackDamage) hero.attackDamage = monster.attackDamage
+        println(s"モンスターは倒れた。")
+
+        if (hero.attackDamage < monster.attackDamage) {
+          hero.attackDamage = monster.attackDamage
+          println(s"そして${hero.name}は、モンスターの武器を奪い攻撃力が${monster.attackDamage}に上がった！")
+        }
       }
+
       monsters = monsters.tail
 
       println(s"残りのモンスターは${monsters.length}匹となった。")
 
-      if (monsters.length > 0) {
+      if (monsters.nonEmpty) {
         println(
           """---------------------------------------------
             |新たな未知のモンスターがあらわれた。 """.stripMargin)
@@ -62,7 +87,7 @@ object RPG extends App {
 
   println(
     s"""---------------------------------------------
-      |【ゲームクリア】: あなたは困難を乗り越えた。新たな冒険に祝福を。
+      |【ゲームクリア】: ${hero.name}は困難を乗り越えた。新たな冒険に祝福を。
       |【結果】: ${hero}""".stripMargin)
 
   System.exit(0)
@@ -71,42 +96,58 @@ object RPG extends App {
 abstract class Creature(
   var hitPoint:     Int,
   var attackDamage: Int
-
 ) {
-  def isAlive(): Boolean = this.hitPoint > 0
+  var name: String = "{{name}}"
+
+  def isAlive: Boolean = this.hitPoint > 0
+
+  def attack(target: Creature): Unit = {
+    target.hitPoint -= this.attackDamage
+  }
+
+  // TODO: add avoid attacks
+
+  // TODO: add defence attacks
 }
 
 class Hero(
-  val name: String,
+  _name:         String,
   _hitPoint:     Int,
   _attackDamage: Int
 
 ) extends Creature (_hitPoint, _attackDamage) {
-  def attack(monster: Monster): Unit = {
-    monster.hitPoint = monster.hitPoint - monster.attackDamage
-    this.hitPoint    = this.hitPoint - monster.attackDamage
-  }
+  name = _name
 
-  def escape(monster: Monster): Boolean = {
+  def isEscape(monster: Monster): Boolean = {
     val isEscaped = RPG.random.nextInt(2) == 1
-
-    if (!isEscaped) {
-      this.hitPoint = this.hitPoint - monster.attackDamage
-    } else {
-      monster.isAwayFromHero = true
-    }
     isEscaped
   }
 
-  override def toString = s"${name}(体力:${hitPoint}, 攻撃力:${attackDamage})"
+  override def toString =
+    s"${name}(体力:${if (hitPoint > 0) hitPoint else 0}, 攻撃力:${attackDamage})"
 }
 
 class Monster(
-  _hitPoint: Int,
+  _hitPoint:     Int,
   _attackDamage: Int,
   var isAwayFromHero: Boolean
 
 ) extends Creature(_hitPoint, _attackDamage) {
-  override def toString = s"Monster(体力:${hitPoint}, 攻撃力:${attackDamage}, ヒーローから離れている:${isAwayFromHero})"
+  val nameList: Seq[String] = Seq(
+    "hogehoge",
+    "fugafuga",
+    "dosudosu",
+    "gasugasu",
+    "pokupoku"
+  )
+  name = setName()
+
+  def setName(): String = {
+    val key = RPG.random.nextInt(5)
+    nameList(key)
+  }
+
+  override def toString =
+    s"${name}(体力:${if (hitPoint > 0) hitPoint else 0}, 攻撃力:${attackDamage}, ヒーローから離れている:${isAwayFromHero})"
 
 }
