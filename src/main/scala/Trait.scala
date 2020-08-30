817007d9da18db545d341e570d26aa62c1ca4815
ko-kamenashi <kamenashi0917@gmail.com>
2020-08-30 11:09:17 +0900

    Lesson15: Scala's Trait inheritance order
    enable to output in order by using super

--- src/main/scala/Trait.scala
+++ src/main/scala/Trait.scala
@@ -17,3 +17,32 @@ trait TraitC extends TraitA {
  * ex. (new Class1).greet() >> Good evening!
  */
 class Class1 extends  TraitC with TraitB
+
+trait TraitD {
+  def greet(): Unit = println("Hello!")
+}
+
+trait TraitE extends TraitD {
+  override def greet(): Unit = {
+    super.greet()
+    println("My name is Terebi-chan")
+  }
+}
+
+trait  TraitF extends  TraitD {
+  override def greet(): Unit = {
+    super.greet()
+    println("I like niconico.")
+  }
+}
+/**
+ * enable to output in order by using super
+ *
+ * ex. (new Class2).greet() >> """
+ *   |Hello!
+ *   |My name is Terebi-chan
+ *   |I like niconico."""
+ */
+
+class Class2 extends TraitE with TraitF
+class Class3 extends TraitF with TraitE
